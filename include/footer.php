781b24cb74907ee40bcec4bbb0c593d23c7761e2
shivanshaggarwal <bhupeshamazon2072@gmail.com>
2025-08-16 20:56:27 +0530

    faq's

--- include/footer.php
+++ include/footer.php
@@ -105,7 +105,7 @@
                           </li>
                           <li>
                               <a href="faq.php">
-                                  FAQ’s
+                                  FAQs
                                   <svg width="6" height="6" viewBox="0 0 6 6" xmlns="http://www.w3.org/2000/svg">
                                       <circle cx="3" cy="3" r="3" />
                                   </svg>
